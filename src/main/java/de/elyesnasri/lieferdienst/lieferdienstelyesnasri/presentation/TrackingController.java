0efca2d1e72e239fb930506a709d96b895164ee1
elyesnasri <m.nasri@pure-apps.de>
2020-01-18 20:42:00 +0100

    implemented error message if order data are wrong

--- src/main/java/de/elyesnasri/lieferdienst/lieferdienstelyesnasri/presentation/TrackingController.java
+++ src/main/java/de/elyesnasri/lieferdienst/lieferdienstelyesnasri/presentation/TrackingController.java
@@ -3,14 +3,20 @@ package de.elyesnasri.lieferdienst.lieferdienstelyesnasri.presentation;
 import de.elyesnasri.lieferdienst.lieferdienstelyesnasri.application.orderService.IOrderService;
 import de.elyesnasri.lieferdienst.lieferdienstelyesnasri.persistence.entities.Order;
 import de.elyesnasri.lieferdienst.lieferdienstelyesnasri.persistence.entities.SearchOrder;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -29,20 +35,25 @@ public class TrackingController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/tracking")
-    public String searchOrder(@ModelAttribute("searchOrder") SearchOrder searchOrder, RedirectAttributes redirectAttributes) {
-        Optional<Order> order;
+    public String searchOrder(@ModelAttribute("searchOrder") SearchOrder searchOrder, Model model, RedirectAttributes redirectAttributes) {
         String lastName = searchOrder.getLastName();
-        int plz = searchOrder.getPlz();
-        String code;
+        int postalCode = searchOrder.getPlz();
 
+        Optional<Order> order = this.orderService.getParcelByNumber(searchOrder.getParcelNumber());
+        if (order.isPresent()) {
+            String getLastName = order.get().getRecipient().getPersonalData().getLastName();
+            int getPostalCode = order.get().getRecipient().getPersonalData().getAddress().getPostalCode();
 
-
-        if ((searchOrder.getParcelNumber() != null) || (!searchOrder.getParcelNumber().isEmpty())) {
-            order = this.orderService.getParcelByNumber(searchOrder.getParcelNumber());
+            if (!getLastName.equals(lastName) || (getPostalCode != postalCode)) {
+                model.addAttribute("error", "Es liegen keine Infromationen für die Sendung vor.");
+                return "tracking";
+            }
         } else {
-            order = this.orderService.getParcelByNamePLZ(lastName, plz);
+            model.addAttribute("error", "Es liegen keine Infromationen für die Sendung vor.");
+            return "tracking";
         }
-        // TODO search for the order per number or name+plz
-        return "redirect:/trackingstatus/" +searchOrder.getParcelNumber();
+
+        // TODO show error not found
+        return "redirect:/trackingstatus/" + order.get().getParcelNumber();
     }
 }
